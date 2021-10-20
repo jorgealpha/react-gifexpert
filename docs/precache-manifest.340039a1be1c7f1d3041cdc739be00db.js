d9caf0511b9ef34b4cde26afe50c0d34608dbe9d
Jorge <jorgemario0728@gmail.com>
2021-10-20 12:24:29 -0600

    Rename buil to docs

---
+++ docs/precache-manifest.340039a1be1c7f1d3041cdc739be00db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af68ce2d751f75e7e33e16aa546b13a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e833cef512f9838b9657",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e833cef512f9838b9657",
+    "url": "/static/js/main.7b42554a.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
